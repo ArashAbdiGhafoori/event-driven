35a57816299c0e4ab2c01e80ea10187604e37442
Arash Abdi Ghafoori <arash.abdi.gh@gmail.com>
2023-06-22 21:24:19 +0430

    add tests for dependency injection
    test dependency injection

---
+++ test/unit/services/get.test.ts
@@ -0,0 +1,49 @@
+import Container from "../../../src/container";
+import evma from "../../../src/main";
+import Service from "../../../src/types/di/service";
+
+let container: Container;
+let counter = 0;
+beforeEach(() => {
+  container = evma.container(`${counter++}`, true);
+});
+
+test("singleton should be available through container.get.service(<service-name>)", () => {
+  const serviceName = "ServiceName";
+  const expected = "ServiceTest";
+  const serviceInstance = { test: expected };
+  container.register.service({
+    name: serviceName,
+    life: "Singleton",
+    instance: serviceInstance,
+  });
+
+  const actual = container.get.service(serviceName) as Service<{
+    test: string;
+  }>;
+  expect(actual).toBeDefined();
+  expect(actual.instance).toBeDefined();
+  expect(actual.instance?.test).toBe(expected);
+});
+
+test("transient should be available through container.get.service(<service-name>)", () => {
+  const serviceName = "ServiceName";
+  const expected = "ServiceTest";
+  container.register.service<Service<string>, string>({
+    name: serviceName,
+    life: "Transient",
+    factory() {
+      return expected;
+    },
+  });
+
+  const actual = container.get.service(serviceName) as Service<{
+    test: string;
+  }>;
+  expect(actual).toBeDefined();
+  expect(actual.factory).toBeDefined();
+  if (actual.factory) {
+    expect(actual.factory()).toBeDefined();
+    expect(actual.factory()).toBe(expected);
+  }
+});
