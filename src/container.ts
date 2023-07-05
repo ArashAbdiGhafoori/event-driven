dbac7f16bc555ed7df77b372da36e9c9864186c6
Arash Abdi Ghafoori <arash.abdi.gh@gmail.com>
2023-07-05 21:05:06 +0430

    perf(container): merge all the maps into one
    merge all the mapps into one in order to reduce the size of output

--- src/container.ts
+++ src/container.ts
@@ -16,13 +16,15 @@ import type {
 
 import { safe_not_equal, noop } from "./util";
 
+export interface StoreEntry {
+  type: "readable" | "writable" | "event" | "pipeline" | "handler" | "service";
+  value: unknown;
+}
+
 export default class Container {
-  //#region di
-  private items: Map<string, unknown> = new Map<string, unknown>();
-  //#endregion
+  private store: Map<string, StoreEntry> = new Map<string, StoreEntry>();
 
   //#region event
-  private events = new Map<string, Event<unknown>>();
 
   /** Listen to the specified event. optionally you can register it here.
    *
@@ -37,13 +39,13 @@ export default class Container {
     register = false,
     count = -1
   ): void {
-    let event = null;
-    if (register && !this.events.has(name)) {
+    let event = this.resolve<Event<T>>(name, "event");
+    if (register && !event) {
       this.register.event(name, count);
+      event = this.resolve<Event<T>>(name, "event");
     }
-    event = this.events.get(name);
     if (event) {
-      event.listeners.push(callback as Listener<unknown>);
+      event.listeners.push(callback as Listener<T>);
     }
   }
 
@@ -53,7 +55,7 @@ export default class Container {
    * @param eventData The Date to fire the event with.
    */
   public fire<T>(name: string, eventData: T): void {
-    const event = this.events.get(name);
+    const event = this.resolve<Event<T>>(name, "event");
     if (!event || event.listeners.length <= 0 || event.count === 0) return;
     for (let i = 0; i < event.listeners.length; i++) {
       const listener = event.listeners[i];
@@ -67,27 +69,25 @@ export default class Container {
    * @param name The name of the event, which it's count should reduce.
    */
   private reduceEventCount(name: string): void {
-    const event = this.events.get(name);
+    const event = this.resolve<Event<unknown>>(name, "event");
     if (event) event.count > 0 ? event.count-- : null;
   }
   //#endregion
 
   //#region cqrs
-  private handlers = new Map<string, Handler<Request<unknown>, unknown>>();
 
   /** Sends a request to handle.
    *
    * @param request The request to handle.
    * @returns The response of handler.
    */
-  public handle<T extends Request<J>, J>(request: T): J {
-    const handler = this.handlers.get(request.name) as Handler<T, J>;
-    return handler.handle(request);
+  public handle<T extends Request<J>, J>(request: T): J | undefined {
+    const handler = this.resolve<Handler<T, J>>(request.name, "handler");
+    return handler?.handle(request);
   }
   //#endregion
 
   //#region pipline
-  private pipelines = new Map<string, Pipeline<unknown>>();
 
   /** Pipe a value to the specified pipeline and get the output.
    *
@@ -96,8 +96,8 @@ export default class Container {
    * @returns The output.
    */
   public pipe<T>(pipeline: string, input: T): T {
-    const line = this.pipelines.get(pipeline) as Pipeline<T>;
-    line.pipes.forEach((p) => {
+    const line = this.resolve<Pipeline<T>>(pipeline, "pipeline");
+    line?.pipes.forEach((p) => {
       input = p(input);
     });
     return input;
@@ -105,7 +105,6 @@ export default class Container {
   //#endregion
 
   //#region store
-  private stores = new Map<string, Writable<unknown> | Readable<unknown>>();
 
   /** Creates a writable store.
    *
@@ -175,139 +174,170 @@ export default class Container {
   }
   //#endregion
 
-  /** Get service, writable or readable stores.
+  /** Resolves entries to types
+   *
+   * @param name The name of the entry to resolve.
+   * @param type The type to resolve
+   * @returns The resolved entry.
    */
-  public get get() {
-    return {
-      /** Get a readable store.
-       *
-       * @param name The name of the readable store
-       * @returns The store
-       */
-      readable: <T>(name: string) => this.stores.get(name) as Readable<T>,
-
-      /** Get a writable store.
-       *
-       * @param name The name of the writable store
-       * @returns The store
-       */
-      writable: <T>(name: string) => this.stores.get(name) as Writable<T>,
-
-      /** Get an instance of a service
-       *
-       * @param name The name of the service
-       * @returns The service
-       */
-      service: <T>(name: string) => {
-        const service: ServiceContainer<T> = this.items.get(
-          name
-        ) as ServiceContainer<T>;
-        const factory = service?.factory;
-        if (service && service.life == "Singleton") return service.instance;
-        if (factory) return factory();
-      },
-    };
+  private resolve<T>(
+    name: string,
+    type: "readable" | "writable" | "event" | "pipeline" | "handler" | "service"
+  ) {
+    const entry = this.store.get(name);
+    if (entry && entry.type == type && entry.value) {
+      return entry.value as T;
+    }
   }
 
-  /** Register to the container.
-   */
-  public get register() {
-    return {
-      /** Register a service
-       *
-       * @param service The implementation of ServiceContainer<TService>
-       */
-      service: <T extends ServiceContainer<J>, J>(service: T) => {
-        if (!this.items.has(service.name)) {
-          this.items.set(service.name, service);
-        }
-      },
-      /** Register a cqrs handler
-       *
-       * @param name The name of handler
-       * @param handle The callback to handle the request
-       */
-      handler: <T extends Request<J>, J>(
-        name: string,
-        handle: (request: T) => J
-      ) => {
-        this.handlers.set(name, { name, handle });
-      },
-      /** Register a pipeline
-       *
-       * @param name The name of the pipeline
-       */
-      pipeline: (name: string) => {
-        this.pipelines.set(name, { name, pipes: [] });
-      },
+  private get_readable_store = <T>(name: string) => {
+    return this.resolve<Readable<T>>(name, "readable");
+  };
 
-      /** Register pipe
-       *
-       * @param pipeline The name of the pipeline to add the pipe to
-       * @param pipe The pipe
-       * @param at The position to add the pie at, default = 0
-       * @param register Register the pipe if not already there
-       */
-      pipe: <T>(
-        pipeline: string,
-        pipe: Pipe<T>,
-        at = 0,
-        register = false
-      ): void => {
-        if (!this.pipelines.has(pipeline) && !register) {
-          return;
-        }
-        if (!this.pipelines.has(pipeline)) {
-          this.register.pipeline(pipeline);
-        }
-        const pl = this.pipelines.get(pipeline);
-        if (pl) pl.pipes.splice(at, 0, pipe as Pipe<unknown>);
-      },
-      // FIXME Invalidation & Rapid changes
-      /** Register a readable store
-       *
-       * @param name The name of the store
-       * @param value The initial value
-       * @param start The notifier
-       */
-      readable: <T>(
-        name: string,
-        value?: T,
-        start: StartStopNotifier<T> = noop
-      ): void => {
-        const readable = this.readable(name, value, start);
-        this.stores.set(name, readable);
-      },
-      // FIXME Invalidation & Rapid changes
-      /** Register a writable store
-       *
-       * @param name The name of the store
-       * @param value The initial value
-       * @param start The notifier
-       */
-      writable: <T>(
-        name: string,
-        value?: T,
-        start: StartStopNotifier<T> = noop
-      ): void => {
-        const writable = this.writable(name, value, start);
-        this.stores.set(name, writable);
-      },
+  private get_writable_store = <T>(name: string) => {
+    return this.resolve<Writable<T>>(name, "writable");
+  };
 
-      /**
-       *
-       * @param name The name of the event
-       * @param count The number of times the event runs, default = -1 (unlimited)
-       */
-      event: (name: string, count = -1) => {
-        this.events.set(name, { name, count, listeners: [] });
-      },
-    };
-  }
+  private get_service = <T>(name: string) => {
+    const service = this.resolve<ServiceContainer<T>>(name, "service");
+    const factory = service?.factory;
+    if (service && service.life == "Singleton") return service.instance;
+    if (factory) return factory();
+  };
 
-  //#region aliases
-  public service = this.get.service;
-  public wstore = this.get.writable;
-  public rstore = this.get.readable;
-  //#endregion
+  public readonly get = {
+    /** Get a readable store.
+     *
+     * @param name The name of the readable store
+     * @returns The store
+     */
+    readable: this.get_readable_store,
+
+    /** Get a writable store.
+     *
+     * @param name The name of the writable store
+     * @returns The store
+     */
+    writable: this.get_writable_store,
+
+    /** Get an instance of a service
+     *
+     * @param name The name of the service
+     * @returns The service
+     */
+    service: this.get_service,
+  };
+
+  // FIXME Invalidation & Rapid changes
+  private register_writable = <T>(
+    name: string,
+    value?: T,
+    start: StartStopNotifier<T> = noop
+  ) => {
+    const writable = this.writable(name, value, start);
+    this.store.set(name, { type: "writable", value: writable });
+  };
+
+  // FIXME Invalidation & Rapid changes
+  private register_readable = <T>(
+    name: string,
+    value?: T,
+    start: StartStopNotifier<T> = noop
+  ) => {
+    const readable = this.readable(name, value, start);
+    this.store.set(name, { type: "readable", value: readable });
+  };
+
+  private register_service = <T extends ServiceContainer<J>, J>(service: T) => {
+    if (!this.store.has(service.name)) {
+      this.store.set(service.name, { type: "service", value: service });
+    }
+  };
+
+  private register_handler = <T extends Request<J>, J>(
+    name: string,
+    handle: (request: T) => J
+  ) => {
+    this.store.set(name, { type: "handler", value: { name, handle } });
+  };
+
+  private register_pipeline = (name: string) => {
+    this.store.set(name, { type: "pipeline", value: { name, pipes: [] } });
+  };
+
+  private register_pipe = <T>(
+    pipeline: string,
+    pipe: Pipe<T>,
+    at = 0,
+    register = false
+  ) => {
+    if (!this.store.has(pipeline) && !register) {
+      return;
+    }
+    if (!this.store.has(pipeline)) {
+      this.register_pipeline(pipeline);
+    }
+    const pl = this.resolve<Pipeline<unknown>>(pipeline, "pipeline");
+    if (pl) pl.pipes.splice(at, 0, pipe as Pipe<unknown>);
+  };
+
+  private register_event = (name: string, count = -1) => {
+    this.store.set(name, {
+      type: "event",
+      value: { name, count, listeners: [] },
+    });
+  };
+
+  public readonly register = {
+    /** Register a writable store
+     *
+     * @param name The name of the store
+     * @param value The initial value
+     * @param start The notifier
+     */
+    writable: this.register_writable,
+
+    /** Register a readable store
+     *
+     * @param name The name of the store
+     * @param value The initial value
+     * @param start The notifier
+     */
+    readable: this.register_readable,
+
+    /** Register a service
+     *
+     * @param service The implementation of ServiceContainer<TService>
+     */
+    service: this.register_service,
+
+    /** Register a cqrs handler
+     *
+     * @param name The name of handler
+     * @param handle The callback to handle the request
+     */
+    handler: this.register_handler,
+
+    /** Register a pipeline
+     *
+     * @param name The name of the pipeline
+     */
+    pipeline: this.register_pipeline,
+
+    /** Register pipe
+     *
+     * @param pipeline The name of the pipeline to add the pipe to
+     * @param pipe The pipe
+     * @param at The position to add the pie at, default = 0
+     * @param register Register the pipe if not already there
+     */
+    pipe: this.register_pipe,
+
+    /** Register an event
+     *
+     * @param name The name of the event
+     * @param count The number of times the event runs, default = -1 (unlimited)
+     */
+    event: this.register_event,
+  };
 }
